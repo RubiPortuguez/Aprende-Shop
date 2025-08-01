4542821e5be6293f4bd73f18a18aa381a94733dd
Rubi Portuguez <portuguezplata.jazminrubi@gmail.com>
2025-07-31 22:41:44 -0600

    Se agrega la información de Rubi y se ajustan las alturas y alineación de cards

---
+++ javascript/nosotras.js
@@ -0,0 +1,13 @@
+
+const copiarCorreo = document.getElementsByClassName('copiarCorreo')
+
+for (let i = 0; i < copiarCorreo.length; i++) {
+    copiarCorreo[i].addEventListener('click', function(event){
+        event.preventDefault();
+        const correoPersona = this.dataset.correo;
+
+        navigator.clipboard.writeText(correoPersona).then(function(){
+            alert('Correo copiado al portapapeles');
+        })
+    });
+};
