0a650f8a4452b8493b6a672c6ee11659f31e456d
Yessica <yessiiramirez98@gmail.com>
2025-08-16 19:22:45 -0600

    Se genero carrusel de comentario en el datalle del producto

--- javascript/clasesCatalogo.js
+++ javascript/clasesCatalogo.js
@@ -8,7 +8,7 @@ function addItem(product) {
     
     const card = `
     <div class="col-sm-6 col-md-4 col-lg-3">
-        <div class="card h-100 shadow-sm">
+        <div class="card h-100 shadow-sm clases">
             <img src="${product.mainImage}" class="card-img-top" alt="${product.alt}">
             <button id="corazon"> <i class="bi bi-heart-fill"></i> </button>
             <button id="corazon2"> <i class="bi bi-heart"></i> </button>
